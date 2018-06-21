aa96460a7a88ec1e329dcf3335a712d5cb47fb59
Tanase Hagi <TanaseHagi@users.noreply.github.com>
2018-06-21 16:40:41 +0300

    added db support

--- Startup.cs
+++ Startup.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using ASP.NET_2_1_AUTH_API_EXAMPLE.Models;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -19,22 +21,81 @@ namespace ASP.NET_2_1_AUTH_API_EXAMPLE
     {
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            _config = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        private IConfiguration _config { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<TodoContext>(opt => 
-                opt.UseInMemoryDatabase("TodoList"));
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            // services.AddDbContext<ToDoItemContext>(opt =>
+            //     opt.UseInMemoryDatabase("TodoList"));
+
+            services.AddDbContext<ToDoItemContext>(options =>
+                options.UseSqlite(
+                    _config.GetConnectionString("ToDoItemConnection")));
+            
+            services.AddDbContext<ToDoUserContext>(options =>
+                options.UseSqlite(
+                    _config.GetConnectionString("ToDoUserConnection")));
+            
+            // services.AddDefaultIdentity<IdentityUser>()
+            //     .AddEntityFrameworkStores<ToDoUserContext>();
+
+            services.AddIdentity<ToDoUser, IdentityRole>()
+                .AddEntityFrameworkStores<ToDoUserContext>()
+                .AddDefaultTokenProviders();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                // Password settings
+                options.Password.RequireDigit = true;
+                options.Password.RequiredLength = 8;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireLowercase = false;
+                options.Password.RequiredUniqueChars = 6;
+
+                // Lockout settings
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
+                options.Lockout.MaxFailedAccessAttempts = 10;
+                options.Lockout.AllowedForNewUsers = true;
+
+                // User settings
+                options.User.RequireUniqueEmail = true;
+            });
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                // Cookie settings
+                options.Cookie.HttpOnly = true;
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+                // If the LoginPath isn't set, ASP.NET Core defaults 
+                // the path to /Account/Login.
+                options.LoginPath = "/Account/Login";
+                // If the AccessDeniedPath isn't set, ASP.NET Core defaults 
+                // the path to /Account/AccessDenied.
+                options.AccessDeniedPath = "/Account/AccessDenied";
+                options.SlidingExpiration = true;
+            });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddConsole(_config.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -45,6 +106,10 @@ namespace ASP.NET_2_1_AUTH_API_EXAMPLE
             }
 
             app.UseHttpsRedirection();
+            app.UseCookiePolicy();
+
+            app.UseAuthentication();
+            
             app.UseMvc();
         }
     }
